c3b77efa5dfc325f3b4b380b9135f1af8392abb4
Dmitriy Shymko <66622565+securon2017@users.noreply.github.com>
2023-10-16 23:59:54 +0300

    Added TFA.E2E

---
+++ TFA.E2E/ForumEndpointsShould.cs
@@ -0,0 +1,28 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using TFA.API;
+
+namespace TFA.E2E
+{
+    public class ForumEndpointsShould : IClassFixture<ForumApiApplicationFactory>
+    {
+        private readonly WebApplicationFactory<Program> factory;
+
+        public ForumEndpointsShould(ForumApiApplicationFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        [Fact]
+        public async Task CreateNewForum()
+        {
+            using var httpclient = factory.CreateClient();
+            using var response = await httpclient.GetAsync("forums");
+            response.Invoking(r => r.EnsureSuccessStatusCode()).Should().NotThrow();
+
+            var result = await response.Content.ReadAsStringAsync();
+            result.Should().Be("[]");
+        }
+
+    }
+}
